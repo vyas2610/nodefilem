f9642253f178266bc016cebd509fe8c3f9809624
vyas2610 <deepakvyas261092@gmail.com>
2025-05-19 22:18:07 +0530

    FS

---
+++ index.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+// fs.writeFileSync("./sample.txt", "Hello MCA");
+// fs.writeFile("./data.txt", "Hy There", (err) => {});
+// const resp = fs.readFileSync("./Content.txt", "utf-8");
+// console.log(resp);
+
+// fs.readFile("./Content.txt", "utf-8", (err, data) => {
+//   if (err) {
+//     console.log(err.message);
+//   } else {
+//     console.log(data);
+//   }
+// });
